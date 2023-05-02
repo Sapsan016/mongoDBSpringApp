e8b53d2acd2b9c1d736d768f43af3b7d9adccd52
Mikhail Gorbunov <mixalich84@yandex.ru>
2023-05-02 17:21:17 +0300

    feat: Added project structure

---
+++ .idea/src/test/java/ru/gorbunov/MongoDB_app/MongoDbAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gorbunov.MongoDB_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
